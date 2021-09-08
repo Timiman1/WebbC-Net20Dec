108939435eb0d5f6f3d12b5fd60fc44ba2c7b5ca
Michael Gustavsson <michael.gustavsson@skaftoskorpan.se>
2021-09-08 15:07:35 +0200

    Updated datastructures with Sets()

--- JavaScript/data-structures/app.js
+++ JavaScript/data-structures/app.js
@@ -41,7 +41,7 @@ console.log(k, l, z);
 */
 
 //Destructuring objects...
-const car = {
+/*const car = {
   id: 1,
   registrationNo: 'ABC123',
   make: 'Chevrolet',
@@ -51,7 +51,7 @@ const car = {
   mileage: 25000,
   description:
     'Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nam commodo venenatis rhoncus. Vivamus id lacus id elit tristique vulputate. Donec ut diam vitae ligula mollis egestas. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Cras tempor sed elit nec pretium. In accumsan odio id est vehicula tincidunt. Aliquam tincidunt risus nec tellus auctor, eget gravida purus efficitur.',
-};
+};*/
 /*
 //Destructuring object.
 //Bara plocka ut de egenskaper som är intressanta...
@@ -85,3 +85,84 @@ console.log(copyOfArr);
 const copyOfCar = { ...car };
 console.log(copyOfCar);
 */
+
+/* SETS ÄR LISTOR/ARRAYS MED ENDBART UNIKA VÄRDEN */
+//Får inte finnas dubbletter!!!
+//ES6 ...
+
+/*
+const manufacturers = [
+  'Volvo',
+  'Kia',
+  'Fiat',
+  'BMW',
+  'Skoda',
+  'Kia',
+  'Volkswagen',
+  'Kia',
+  'Volvo',
+];
+
+console.log(manufacturers);
+
+const makeSet = new Set(manufacturers);
+console.log(makeSet);
+
+//Kontrollera storleken
+console.log(makeSet.size);
+//Kontrollera om ett specifikt element/objekt finns i listan
+console.log(makeSet.has('Fiat'));
+console.log(makeSet.has('Porsche'));
+
+//Lägga till nya element/objekt.
+makeSet.add('Porsche');
+
+console.log(makeSet);
+console.log(makeSet.has('Porsche'));
+
+//Ta bort element/objekt
+makeSet.delete('Fiat');
+makeSet.delete('volvo');
+console.log(makeSet);
+
+//Ta bort vårt Set
+makeSet.clear();
+console.log(makeSet);
+
+//Vad ska vi ha det till?
+//Eliminera dubbletter som vi kanske får tillbaka via ett REST anrop.
+
+const models = [
+  'V40',
+  'V60',
+  '320i',
+  'Ceed',
+  'Trafic',
+  'Ceed',
+  'V60',
+  'Ceed',
+  'Trafic',
+];
+
+const uniqueModels = new Set(models);
+console.log(uniqueModels);
+
+const newModels = [...uniqueModels];
+console.log(newModels);
+
+//One-liner...
+const modelsArray = [...new Set(models)];
+console.log(modelsArray);
+*/
+
+/* MAPS */
+//Kan liknas vid Dictionaries. Nyckel(key) och värde(value) par.
+const vehicleUsage = new Map();
+vehicleUsage.set('N', 'Normal Use');
+vehicleUsage.set('RC', 'Rental Car');
+vehicleUsage.set('T', 'Taxi');
+vehicleUsage.set('D', 'Driving School');
+
+console.log(vehicleUsage);
+
+console.log(vehicleUsage.get('RC'));
