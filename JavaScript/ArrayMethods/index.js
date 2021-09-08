cfed34bdb28be7bad84395dca70bc2021fc27adf
Michael Gustavsson <michael.gustavsson@skaftoskorpan.se>
2021-09-08 13:41:41 +0200

    Added Array methods

---
+++ JavaScript/ArrayMethods/index.js
@@ -0,0 +1,69 @@
+const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+//console.log(arr);
+
+/* SLICE en metod som tar inga, 1 eller 2 argument */
+//Immutable!!!
+//Plockar element/objekt ifrån en array placerar i en ny array.
+/* const arr1 = arr.slice();
+console.log(arr);
+console.log(arr1);
+
+//Var börjar vi plocka i original arrayen.
+const arr2 = arr.slice(3);
+console.log('Arr 2', arr2);
+
+//Var börjar vi och var slutar vi.
+const arr3 = arr.slice(3, 8);
+console.log('Arr 3', arr3);
+console.log(arr);
+
+const arr4 = arr.slice(-2);
+console.log(arr4);
+console.log(arr);
+*/
+
+//SPLICE() förändrar original arryen.
+//Mutable!
+/*
+console.log(arr);
+const arr5 = arr.splice(4);
+console.log('arr 5', arr5);
+console.log(arr);
+*/
+
+//Oftast för att ta bort ett element eller objekt ifrån en array.
+/*const arr6 = arr.splice(3, 3);
+console.log(arr6);
+console.log(arr);
+*/
+
+/* REVERSE() */
+/*console.log(arr);
+const arr7 = arr.reverse();
+console.log(arr7);
+*/
+
+/* SORT() */
+/*console.log(arr);
+console.log(
+  arr.sort((a, b) => {
+    b - a;
+  })
+);
+*/
+
+/* CONCAT() */
+//Immutable!!!
+/*const arr8 = ['a', 'b', 'c', 'd'];
+console.log(arr8);
+
+const arr9 = arr.concat(arr8);
+console.log(arr9);
+*/
+
+/* JOIN() */
+//Immutable!!!
+/*const arr10 = arr.join(' ');
+console.log(arr10);
+console.log(arr);
+*/
