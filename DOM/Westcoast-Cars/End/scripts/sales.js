d27c64fd5ecd099d046b22d04367cd86a4cd3d0c
Michael Gustavsson <michael.gustavsson@skaftoskorpan.se>
2021-09-09 15:55:21 +0200

    Finished up sales gallery

--- DOM/Westcoast-Cars/End/scripts/sales.js
+++ DOM/Westcoast-Cars/End/scripts/sales.js
@@ -2,6 +2,9 @@
 
 //Skapa en referens variabel till vår section med klassen gallery.
 const gallery = document.querySelector('.gallery');
+const modalDialog = document.querySelector('.modal');
+const closeDialog = document.querySelector('.close-modal');
+const overlay = document.querySelector('.overlay');
 
 //Loopa igenom alla bilar i vehicles listan/array
 // for (let car of vehicles) {
@@ -14,13 +17,61 @@ const gallery = document.querySelector('.gallery');
 //   );
 // }
 
-//skriver om ovanstående till en forEach loop.
+//skriv om ovanstående for let loop till en forEach loop.
 vehicles.forEach((car) => {
   gallery.insertAdjacentHTML(
     'beforeend',
     `<div class="car">
-      <img src="./content/img/${car.imageName}.jpg" alt="${car.make}" />
+      <img id="${car.id}" src="./content/img/${car.imageName}.jpg" alt="${car.make}" />
       <p>${car.make} ${car.model}</p>
   </div>`
   );
 });
+
+const images = document.querySelectorAll('.car img');
+
+images.forEach((image) => {
+  const src = image.getAttribute('src');
+  const id = image.getAttribute('id');
+  //console.log(src, id);
+
+  image.addEventListener('click', () => {
+    //console.log(src, id);
+    //Nu ska vi skapa en modal dialogruta.
+    openModal(src, id);
+  });
+});
+
+/******************************************************************************/
+//HANTERING AV DEN MODALA DIALOGRUTAN
+/******************************************************************************/
+
+closeDialog.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
+
+//document.querySelector('.close-modal').addEventListener('click', closeModal);
+
+//Funktion för att skapa upp den modala dialogrutan.
+function openModal(imageSource, id) {
+  const image = modalDialog.querySelector('.modal-container');
+  image.innerHTML = `<img src="${imageSource}" alt=""/><a class="btn" href="car-detail.html?vehicleId=${id}">Mer Information</a>`;
+
+  overlay.classList.remove('hidden');
+  modalDialog.classList.remove('hidden');
+}
+
+//Funktion för att stänga det modala fönstret om man trycker på ESC knappen...
+document.addEventListener('keydown', (e) => {
+  console.log(e.key);
+  if (e.key === 'Escape') {
+    if (!modalDialog.classList.contains('hidden')) {
+      closeModal();
+    }
+  }
+});
+
+//Funktion för att stänga det modala fönstret...
+function closeModal() {
+  modalDialog.classList.add('hidden');
+  overlay.classList.add('hidden');
+}
